6485f927e532729da8266c0c414e7673332056d5
vince <vincedinardo21@gmail.com>
2022-05-06 17:39:58 +0200

    Show menu

--- main.js
+++ main.js
@@ -1,7 +1,9 @@
 const hamburger = document.querySelector("div.header-right");
+const showMenu = document.querySelector("div.hamburger-menu");
 
 hamburger.addEventListener("click",
     function(){
+        showMenu.style.display = "block";
         console.log("Hide menu");
     }
 );
