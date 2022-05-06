d538f4cd20116e5aeee96aa0e0cd5e424c39f84e
vince <vincedinardo21@gmail.com>
2022-05-06 17:49:24 +0200

    Apertura e chiusura del menu

--- main.js
+++ main.js
@@ -1,10 +1,18 @@
 const hamburger = document.querySelector("div.header-right");
 const showMenu = document.querySelector("div.hamburger-menu");
+const closeMenu = document.querySelector("a.close");
 
+// Mostra il menu blu cliccando sull'hamburger
 hamburger.addEventListener("click",
     function(){
         showMenu.style.display = "block";
-        console.log("Hide menu");
+    }
+);
+
+// Chiudi il menu blu cliccando sulla x
+closeMenu.addEventListener("click",
+    function(){
+        showMenu.style.display = "none";
     }
 );
 
